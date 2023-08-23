f5784bcb93069eca0407265f2c2b5a3bcf8bbc5b
Griffin <liq543@gmail.com>
2023-08-23 13:10:06 -0500

    updated userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,33 @@
-const express = require('express');
-const router = express.Router();
-const { User } = require('../models/User');
+const router = require('express').Router();
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require('../controllers/userController');
 
-router.get('/api/users', async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.json(users);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+// GET all users
+router.get('/', getAllUsers);
+
+// GET a single user by its _id
+router.get('/:id', getUserById);
+
+// POST a new user
+router.post('/', createUser);
+
+// PUT to update a user by its _id
+router.put('/:id', updateUser);
+
+// DELETE to remove a user by its _id
+router.delete('/:id', deleteUser);
+
+// POST to add a new friend to a user's friend list
+router.post('/:userId/friends/:friendId', addFriend);
+
+// DELETE to remove a friend from a user's friend list
+router.delete('/:userId/friends/:friendId', removeFriend);
 
 module.exports = router;
