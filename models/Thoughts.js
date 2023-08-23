79a5d51c4c6774c887b3e867e13e3164eb65eef0
Griffin <liq543@gmail.com>
2023-08-23 13:07:15 -0500

    added some models and routes

---
+++ models/Thoughts.js
@@ -0,0 +1,50 @@
+const { Schema } = require('mongoose');
+const moment = require('moment');
+
+const ReactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: new Schema.Types.ObjectId()
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxLength: 280
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: timestamp => moment(timestamp).format('MMM D, YYYY [at] hh:mm a')
+  }
+});
+
+const ThoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 280
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: timestamp => moment(timestamp).format('MMM D, YYYY [at] hh:mm a')
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  reactions: [ReactionSchema]
+});
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = { Thought, ReactionSchema };
--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose');
+const express = require('express');
+const userRoutes = require('./routes/userRoutes');
 // import .env
 require('dotenv').config();
 
+const app = express();
+
+app.use(express.json());
+app.use(userRoutes);  // Add other route middlewares
+
+
+const PORT = process.env.PORT || 3001;
 // connect to MONGO_URL from .env
 mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
 });
+
